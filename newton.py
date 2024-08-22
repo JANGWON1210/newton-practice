a10fe4db62bb2e414a0bc7ef32f699a8e51b99a7
jangwon_lee <jangwon_lee@berkeley.edu>
2024-08-22 13:27:21 -0700

    add docstring in the files

--- newton.py
+++ newton.py
@@ -1,11 +1,13 @@
 import numpy as np
 
 def dv(x0, func, ep=1e-6):
+    """Calculation the first derivative value of funcion(func) at x0."""
     f_prime = (func(x0 + ep/2) - func(x0 - ep/2))/ep
     
     return f_prime
 
 def ddv(x0, func, ep=1e-6):
+    """Calculation the second derivative value of funcion(func) at x0."""
     f_prime_plus = dv(x0 + ep/2, func)
     f_prime_minus = dv(x0 - ep/2, func)
     
@@ -13,6 +15,7 @@ def ddv(x0, func, ep=1e-6):
 
 
 def newton_mine(x0, func, tol=1e-6, max_iter=1000):
+    """Apply the Newton method to find a point where the function equals zero, starting from \( x_0 \)."""
     x=x0+10
     count_iter =0
     
@@ -30,7 +33,7 @@ def newton_mine(x0, func, tol=1e-6, max_iter=1000):
         count_iter+=1
     return x, func(x)
 
-# Example usage:
+"""For example"""
 if __name__ == "__main__":
     func = lambda x: x**2 + 4*x + 5
     x_min, f_min = newton_mine(0, func)
